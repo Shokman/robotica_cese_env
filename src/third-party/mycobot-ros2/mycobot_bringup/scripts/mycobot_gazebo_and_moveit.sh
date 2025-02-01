629775393c4f8fc37d2bf906a2536d9a3d642794
Javier Choclin <shokman88@gmail.com>
2025-02-01 16:13:17 +0000

    Add configuration files for mycobot_320

--- src/third-party/mycobot-ros2/mycobot_bringup/scripts/mycobot_gazebo_and_moveit.sh
+++ src/third-party/mycobot-ros2/mycobot_bringup/scripts/mycobot_gazebo_and_moveit.sh
@@ -29,7 +29,7 @@ ros2 launch mycobot_gazebo mycobot.gazebo.launch.py \
     yaw:=0.0 &
 
 sleep 15
-ros2 launch mycobot_moveit_config move_group.launch.py &
+ros2 launch mycobot_moveit_config move_group.launch.py robot_name:=$ROBOT_MODEL &
 
 echo "Adjusting camera position..."
 gz service -s /gui/move_to/pose --reqtype gz.msgs.GUICamera --reptype gz.msgs.Boolean --timeout 2000 --req "pose: {position: {x: 1.36, y: -0.58, z: 0.95} orientation: {x: -0.26, y: 0.1, z: 0.89, w: 0.35}}"
